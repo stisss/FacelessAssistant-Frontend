d6795c7d18b71e59fd8fe2c826af4c20ad1181bf
Błażej Stiskun <bstiskun@fis-sst.pl>
2020-11-13 18:10:26 +0100

    Implemented tooltips and responsive previews (partially)

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -13,6 +13,7 @@ import { Constants } from 'src/app/core/shared/Constants';
 export class HomeComponent implements OnInit {
   imageBase64: string;
   isImageLoaded: boolean = false;
+  isImageHorizontal: boolean = false;
   result: Array<Recognition>;
   numberOfDetected: number;
   numberOfRecognized: number;
@@ -25,19 +26,32 @@ export class HomeComponent implements OnInit {
   }
 
   uploadFile($event): void {
+    this.isImageLoaded = false;
+    this.isImageHorizontal = false;
+
     if ($event.target.files) {
       const reader = new FileReader();
       reader.readAsDataURL($event.target.files[0]);
-      reader.onload = (e: any) => this.imageBase64 = e.target.result;
+
+      reader.onload = (e: any) => {
+        const img = new Image();
+        img.src = e.target.result;
+        this.isImageHorizontal = img.width > img.height;
+
+        setTimeout(() => {
+          this.imageBase64 = e.target.result;
+        }, 0);
+      };
+
       this.result = null;
+      this.isImageLoaded = true;
     }
-    this.isImageLoaded = true;
   }
 
   onIdentifyCharacters(): void {
     this.homeService.identifyCharacter(this.imageBase64).subscribe(res => {
       this.result = res.filter((dto: RecognitionDto) => dto.name !== 'Unknown')
-                       .map((dto: RecognitionDto) => new Recognition(dto));
+        .map((dto: RecognitionDto) => new Recognition(dto));
       this.numberOfDetected = res.length;
       this.numberOfRecognized = this.result.length;
     });
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 const MaterialComponents = [
   MatIconModule,
-  MatDialogModule
+  MatDialogModule,
+  MatTooltipModule
 ]
 
 @NgModule({

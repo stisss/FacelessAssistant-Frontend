2306e8721138b60181eacb3f5925f6876295c368
Błażej Stiskun <bstiskun@fis-sst.pl>
2020-11-25 12:01:26 +0100

    Added connection with backend, new endpoints

--- src/app/characters/characters.component.ts
+++ src/app/characters/characters.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Mode } from './characters-filters/mode';
+import { CharactersService } from './characters.service';
 
 @Component({
   selector: 'app-characters',
@@ -11,9 +12,41 @@ export class CharactersComponent implements OnInit {
   readonly gridMode: Mode = Mode.GRID;
   readonly listMode: Mode = Mode.LIST;
 
-  constructor() { }
+  names: Array<string>;
+  actors: Array<string>;
+  houses: Array<string>;
+  cultures: Array<string>;
+
+  constructor(private charactersService: CharactersService) { }
 
   ngOnInit(): void {
+    this.loadNames();
+    this.loadActors();
+    this.loadCultures();
+    this.loadHouses();
   }
 
+  loadNames(): void {
+    this.charactersService.getNames().subscribe(res => {
+      this.names = res;
+    });
+  }
+
+  loadActors(): void {
+    this.charactersService.getActors().subscribe(res => {
+      this.actors = res;
+    });
+  }
+
+  loadCultures(): void {
+    this.charactersService.getCultures().subscribe(res => {
+      this.cultures = res.filter(x => x !== '');
+    });
+  }
+
+  loadHouses(): void {
+    this.charactersService.getHouses().subscribe(res => {
+      this.houses = res.filter(x => x !== '');
+    });
+  }
 }
--- src/app/characters/characters.service.ts
+++ src/app/characters/characters.service.ts
@@ -2,17 +2,34 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { Constants } from '../core/shared/Constants';
 import { CharacterPreviewDto } from './models/character-preview-dto.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CharactersService {
-  readonly url: string = 'http://localhost:5000/Characters';
 
   constructor(private http: HttpClient) { }
 
   getCharactersPreviews(): Observable<Array<CharacterPreviewDto>> {
-    return this.http.get(this.url).pipe(map((res: Array<string>) => res.map(x => JSON.parse(x)))) as Observable<Array<CharacterPreviewDto>>;
+    return this.http.get(Constants.LOCAL_BACKEND_URL.concat(Constants.CHARACTERS_POSTFIX))
+      .pipe(map((res: Array<string>) => res.map(x => JSON.parse(x)))) as Observable<Array<CharacterPreviewDto>>;
+  }
+
+  getNames(): Observable<Array<string>> {
+    return this.http.get(Constants.LOCAL_BACKEND_URL.concat(Constants.NAMES_POSTFIX)) as Observable<Array<string>>;
+  }
+
+  getCultures(): Observable<Array<string>> {
+    return this.http.get(Constants.LOCAL_BACKEND_URL.concat(Constants.CULTURES_POSTFIX)) as Observable<Array<string>>;
+  }
+
+  getActors(): Observable<Array<string>> {
+    return this.http.get(Constants.LOCAL_BACKEND_URL.concat(Constants.ACTORS_POSTFIX)) as Observable<Array<string>>;
+  }
+
+  getHouses(): Observable<Array<string>> {
+    return this.http.get(Constants.LOCAL_BACKEND_URL.concat(Constants.HOUSES_POSTFIX)) as Observable<Array<string>>;
   }
 }

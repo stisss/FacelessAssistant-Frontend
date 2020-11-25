a54a6230eb87d4b2f9f3b30293b3598725b1266d
Błażej Stiskun <bstiskun@fis-sst.pl>
2020-11-25 11:44:51 +0100

    Added endpoints to constants

--- src/app/core/shared/Constants.ts
+++ src/app/core/shared/Constants.ts
@@ -3,4 +3,13 @@ export abstract class Constants {
   public static readonly FACELESS_LOGO_SRC: string = 'assets/icons/faceless-icon.png';
   public static readonly UPLOAD_ICON_SRC: string = 'assets/icons/upload-icon.svg';
   public static readonly CHARACTER_EXAMPLE_SRC: string = 'assets/images/character-example.jpg';
+
+  /* URLs */
+  public static readonly LOCAL_BACKEND_URL: string = 'http://localhost:5000/';
+  public static readonly EXTERNAL_BACKEND_URL: string = 'http://51.83.186.244/';
+  public static readonly CHARACTERS_POSTFIX: string = 'Characters';
+  public static readonly NAMES_POSTFIX: string = 'Names';
+  public static readonly ACTORS_POSTFIX: string = 'Actors';
+  public static readonly CULTURES_POSTFIX: string = 'Cultures';
+  public static readonly HOUSES_POSTFIX: string = 'Houses';
 }
